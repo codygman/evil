b2e403042aeeb0a8e73238a975a0d4522d0e60bb
codygman <codygman.consulting@gmail.com>
2019-11-29 17:48:51 -0600

    remove cond all together

--- evil-tests.el
+++ evil-tests.el
@@ -78,26 +78,24 @@
 (defun evil-tests-run (&optional tests interactive)
   "Run Evil tests."
   (interactive '(nil t))
-  (cond
-   (t
-    ;; We would like to use `ert-run-tests-batch-and-exit'
-    ;; Unfortunately it doesn't work outside of batch mode, and we
-    ;; can't use batch mode because we have tests that need windows.
-    ;; Instead, run the tests interactively, copy the results to a
-    ;; text file, and then exit with an appropriate code.
-    (setq attempt-stack-overflow-recovery nil
-          attempt-orderly-shutdown-on-fatal-signal nil)
+  ;; We would like to use `ert-run-tests-batch-and-exit'
+  ;; Unfortunately it doesn't work outside of batch mode, and we
+  ;; can't use batch mode because we have tests that need windows.
+  ;; Instead, run the tests interactively, copy the results to a
+  ;; text file, and then exit with an appropriate code.
+  (setq attempt-stack-overflow-recovery nil
+        attempt-orderly-shutdown-on-fatal-signal nil)
+  (unwind-protect
+      (progn
+        (ert-run-tests-interactively tests)
+        (with-current-buffer "*ert*"
+          (append-to-file (point-min) (point-max) "test-results.txt")
+          (kill-emacs (if (zerop (ert-stats-completed-unexpected ert--results-stats)) 0 1))))
     (unwind-protect
         (progn
-          (ert-run-tests-interactively tests)
-          (with-current-buffer "*ert*"
-            (append-to-file (point-min) (point-max) "test-results.txt")
-            (kill-emacs (if (zerop (ert-stats-completed-unexpected ert--results-stats)) 0 1))))
-      (unwind-protect
-          (progn
-            (append-to-file "Error running tests\n" nil "test-results.txt")
-            (append-to-file (backtrace-to-string (backtrace-get-frames 'backtrace)) nil "test-results.txt"))
-        (kill-emacs 2))))))
+          (append-to-file "Error running tests\n" nil "test-results.txt")
+          (append-to-file (backtrace-to-string (backtrace-get-frames 'backtrace)) nil "test-results.txt"))
+      (kill-emacs 2))))
 
 (defun evil-tests-profiler (&optional force)
   "Profile Evil tests."
